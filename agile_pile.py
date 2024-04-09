e6126c137568a9ec33d86333eae0c018962a991c
structcode666 <155280993+structcode666@users.noreply.github.com>
2024-04-10 06:18:13 +1000

    Intermediate Commit 1
    Working Concept of pile load calculator that gives axial loads and shears for loads about the COS of the pile group

--- agile_pile.py
+++ agile_pile.py
@@ -16,17 +16,17 @@ st.write("This application helps visualize the locations of piles under a raft a
 with st.sidebar:
     st.header("PILE INFORMATION")
     no_of_piles = st.text_input("Number of piles under the raft", " ")
-    pile_dia = st.number_input("Pile Diameter", min_value=0)
+    pile_dia = st.number_input("Pile Diameter (mm)", min_value=0)
 st.subheader("Pile Coordinates")
 
 with st.sidebar:
     st.header("LOAD INFORMATION")
-    axial_load = st.text_input("Axial Load (N)", " ")
-    m_xx = st.text_input("Mxx (N.m)", " ")
-    m_yy = st.text_input("Myy (N.m)", " ")
-    # m_zz = st.text_input("Mzz", " ")
-    # v_x = st.text_input("Vx", " ")
-    # v_y = st.text_input("Vy", " ")
+    axial_load = st.text_input("Axial Load (kN)", " ")
+    m_xx = st.text_input("Mxx (kN.m)", " ")
+    m_yy = st.text_input("Myy (kN.m)", " ")
+    m_zz = st.text_input("Mzz", " ")
+    v_x = st.text_input("Vx", " ")
+    v_y = st.text_input("Vy", " ")
 
 ## Set up Pile Data Table##
 pile_data = []
@@ -36,16 +36,17 @@ for i in range(0, int(no_of_piles)):
     pile_coord = (f"P{i+1}" , x , y)
     pile_data.append(pile_coord)
 
-pile_df = pd.DataFrame(pile_data, columns = ["Pile Number" , "X Coord", "Y Coord"])
+pile_df = pd.DataFrame(pile_data, columns = ["Pile Number" , "X Coord (mm)", "Y Coord (mm)"])
 final_pile_df = st.data_editor(pile_df,hide_index=True, num_rows="dynamic")
 
 ## Plot Pile Layout##
 st.subheader("Pile Layout")  
+
 x_coord_acc = []
-for x in final_pile_df["X Coord"]:
+for x in final_pile_df["X Coord (mm)"]:
     x_coord_acc.append(int(x))
 y_coord_acc = []
-for y in final_pile_df["Y Coord"]:
+for y in final_pile_df["Y Coord (mm)"]:
     y_coord_acc.append(int(y))
 
 sec, geom = ap.create_pile_group(x_coord_acc, y_coord_acc, int(no_of_piles), int(pile_dia))
@@ -63,6 +64,7 @@ fig = ax.get_figure()
 
 # plot the centroids
 sec.plot_centroids(ax=fig.axes[0])
+
 st.pyplot(fig)
 
 ##Show pile group properties as a table##
@@ -75,21 +77,26 @@ pile_prop_table = st.data_editor(pile_prop_df,hide_index=True, num_rows="static"
 
 st.subheader("Pile Loads")
 
-load_case = {"n": int(axial_load),
-             "mxx": int(m_xx),
-             "myy": int(m_yy),
-             "mzz": 0.,
-             "vx": 0.,
-             "vy": 0.,
+load_case = {"n": float(axial_load)*1000,
+             "mxx": (float(m_xx))*1000000,
+             "myy": (float(m_yy))*1000000,
+            #  "mzz": 0.,
+            #  "vx": 0.,
+            #  "vy": 0.,
              }
 
-pile_load_data = ap.get_pile_group_loads(int(pile_dia), sec, load_case, list(zip(x_coord_acc, y_coord_acc)))
+pile_load_data = ap.get_pile_group_loads(float(pile_dia), sec, load_case, list(zip(x_coord_acc, y_coord_acc)))
 
 pile_load_df = pd.DataFrame(pile_load_data, columns = ["Pile Vert Reactions" , "Value"])
 
 final_pile_load_df = st.data_editor(pile_load_df, hide_index=True, num_rows="dynamic")
 
 
+shear_load_acc = ap.get_pile_group_shear_loads(sec, float(v_x), float(v_y), list(zip(x_coord_acc, y_coord_acc)), pile_prop_data, int(pile_dia), int(m_zz))
+
+pile_shear_load_df = pd.DataFrame(shear_load_acc, columns = ["Pile Shear Loads", "Shear Load (kN)"] )
+
+final_pile_shaer_load_df = st.data_editor(pile_shear_load_df, hide_index=True, num_rows="dynamic")
 
              
         
--- agile_pile_calculator.py
+++ agile_pile_calculator.py
@@ -19,7 +19,7 @@ def create_pile_group(x_coord_acc = [], y_coord_acc = [], no_of_piles = int, pil
         pile_group +=pile
 
     geom = pile_group
-    geom.create_mesh(mesh_sizes=[30])
+    geom.create_mesh(mesh_sizes=[2000])
     sec = Section(geometry = geom)
     sec.calculate_geometric_properties()
     return sec, geom
@@ -48,23 +48,24 @@ def get_pile_group_loads(pile_dia, sec, load_case = {},  list_of_coords = []):
 
     pile_load_data = []
     for pile_number, stress in enumerate(sig):
-        pile_load = (f"P{pile_number+1}" , stress[0] * area)
+        pile_load = (f"P{pile_number+1}" , round(stress[0] * area/1000))
         pile_load_data.append(pile_load)
 
     return pile_load_data
 
 
+def get_pile_group_shear_loads(sec, v_x: float, v_y : float, list_of_coords : [], pile_prop_data :[], pile_dia : float, m_zz = float):
 
-
-
-
-
-
-
-         
-
-        
-
-
-
+    area = (m.pi * pile_dia**2)*0.25
+    Izz_c = pile_prop_data[3][1]
+    J = Izz_c/area
+    no_of_piles = len(list_of_coords)
+    x_c, y_c = sec.get_c()
+    # Get shear loads per pile##
+    shear_load_acc = []
+    for pile_number, coord in enumerate(list_of_coords):
+        shear_load_pile = (f"P{pile_number+1}", (round((((v_x/no_of_piles)+((m_zz*(coord[0] - x_c)*10**3)/J))**2 + ((v_y/no_of_piles)-((m_zz*(coord[1] - y_c)*10**3)/J))**2)**0.5)))
+        shear_load_acc.append(shear_load_pile)
+    return shear_load_acc
+ 
     
